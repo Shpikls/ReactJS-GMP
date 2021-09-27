5eaada2749e5ad4cb9fd3b4c227ea6e94d56d99d
Aleksey Petsevich <shpikls@shpikls.ru>
2021-09-27 15:48:02 +0500

    fix: Домашнее задание #4 фикс прыгающего контента и реализация хуков

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useEffect } from 'react'
 import styled from 'styled-components'
 import modalCloseButton from '~/asserts/modal-close-button.svg'
 
@@ -9,9 +10,29 @@ type PropsModal = {
 }
 
 export const Modal = ({ isOpen, children, closeModalHandler }: PropsModal): JSX.Element | null => {
-	if (isOpen) {
-		document.body.classList.add('modal-open')
+	useEffect(() => {
+		if (isOpen) {
+			document.body.classList.add('modal-open')
+		} else {
+			document.body.classList.remove('modal-open')
+		}
+	}, [isOpen])
 
+	useEffect(() => {
+		const listener = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				closeModalHandler()
+			}
+		}
+
+		document.addEventListener('keydown', listener)
+
+		return () => {
+			document.removeEventListener('keydown', listener)
+		}
+	}, [])
+
+	if (isOpen) {
 		return (
 			<ModalWrapper>
 				<ModalWindow>
@@ -24,20 +45,19 @@ export const Modal = ({ isOpen, children, closeModalHandler }: PropsModal): JSX.
 		)
 	}
 
-	document.body.classList.remove('modal-open')
-
 	return null
 }
 
 const ModalWrapper = styled.div`
-	width: 100%;
+	width: 100vw;
 	height: 100%;
 	position: fixed;
 	top: 0;
 	left: 0;
 	background: rgba(35, 35, 35, 0.9);
 	color: white;
-	overflow: auto;
+	overflow-x: hidden;
+	overflow-y: auto;
 `
 
 const ModalWindow = styled.div`
@@ -46,14 +66,13 @@ const ModalWindow = styled.div`
 	background: #232323;
 	padding: 60px;
 	box-sizing: border-box;
-	position: relative;
-	top: 0;
-	right: 0;
-	bottom: 0;
-	left: 0;
-	margin: 16px auto;
 	box-shadow: 0 0 2px rgba(0, 0, 0, 0.1), 0 2px 10px rgba(0, 0, 0, 0.07), 0 10px 20px rgba(0, 0, 0, 0.05),
 		0 10px 50px rgba(0, 0, 0, 0.05);
+
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 `
 
 const ModalClose = styled.button`
