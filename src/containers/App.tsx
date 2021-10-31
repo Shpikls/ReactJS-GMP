c0234675d2dc797743bc7af0769a11db1970efb8
Aleksey Petsevich <45347234+Shpikls@users.noreply.github.com>
2021-10-31 11:07:56 +0300

    fix: восстановление работы модальных окон Add, Edit, Delete (#18)

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,12 @@
 import { Spinner } from '@components/Spinner'
+import { DeleteContainer } from '@containers/DeleteContainer'
+import { EditContainer } from '@containers/EditContainer'
 import * as React from 'react'
 import { useSelector } from 'react-redux'
 import { AppLayout } from '~/components/AppLayout'
 import { useGetData } from '~/hooks/useGetData'
 import { appSelectors } from '~/redux/appSlice'
+import { AddContainer } from './AddContainer'
 
 export const App = (): JSX.Element => {
 	const isLoading = useSelector(appSelectors.loading)
@@ -13,6 +16,9 @@ export const App = (): JSX.Element => {
 	return (
 		<>
 			{isLoading && <Spinner />}
+			<AddContainer />
+			<EditContainer />
+			<DeleteContainer />
 			<AppLayout />
 		</>
 	)
